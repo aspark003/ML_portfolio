adc83555774050a650daceac2386b88dbbf66320
Antonio Park <antonio.park0517@outlook.com>
2026-01-12 19:08:10 -0500

    Update full_script.py

--- anomaly_fraud/full_script.py
+++ anomaly_fraud/full_script.py
@@ -38,17 +38,13 @@ class A:
                        ('pca', PCA(n_components=0.9)),
                        ('dbscan', DBSCAN(eps=0.4, min_samples=8))])
 
-        op = Pipeline([('preprocessor', preprocessor),
-                       ('pca', PCA(n_components=0.8)),
-                       ('optics', OPTICS(min_samples=6, xi=0.01))])
-
         hd = Pipeline([('preprocessor', preprocessor),
                        ('pca', PCA(n_components=0.8)),
                        ('hdbscan', HDBSCAN(min_samples=5, min_cluster_size=7, copy=False))])
 
         iso = Pipeline([('preprocessor', preprocessor),
-                        ('pca', PCA(n_components=0.9)),
                         ('iso', IsolationForest(n_estimators=100, max_samples='auto', contamination='auto', random_state=42))])
+
         scores = [silhouette_score, calinski_harabasz_score, davies_bouldin_score]
 
         db.fit(self.df)
@@ -56,22 +52,13 @@ class A:
         x_pca = db.named_steps['pca'].transform(x)
         variance = db.named_steps['pca'].explained_variance_ratio_
         cumsum = np.cumsum(variance)
-        #print(variance)
-        #print(cumsum)
+        print(variance)
+        print(cumsum)
+        
         y = db.named_steps['dbscan'].labels_
         for sc in scores:
             s = sc(x_pca, y)
             print(f'dbscan: {sc.__name__}:{s}')
-        print()
-
-
-        op.fit(self.df)
-        ox = op.named_steps['preprocessor'].transform(self.df)
-        ox_pca = op.named_steps['pca'].transform(ox)
-        oy = op.named_steps['optics'].labels_
-        for oyscore in scores:
-            oys = oyscore(ox_pca, oy)
-            print(f'optics: {oyscore.__name__}:{oys}')
         print()
 
         hd.fit(self.df)
@@ -83,33 +70,39 @@ class A:
             print(f'hdbscan: {ys.__name__}:{ysco}')
         print()
 
+        self.df['hdbscan label'] = yh
+
+        self.df['outlier flag'] = self.df['hdbscan label'] == -1
+
         prob = hd.named_steps['hdbscan'].probabilities_
 
+        self.df['probability score'] = - prob
+        self.df['probability score scaled'] = MinMaxScaler().fit_transform(self.df['probability score'].values.reshape(-1,1)).ravel()
 
-        self.df['hdbscan label'] = yh
-        self.df['outlier'] = (self.df['hdbscan label'].apply(lambda x: 1 if x == -1 else 0))
-        self.df['outlier label'] = (self.df['outlier'].apply(lambda x: 'review' if x == 1 else 'no issues'))
+        self.df['probability level'] = np.where((self.df['outlier flag'] == True) & (self.df['probability score scaled'] > 0.75),'high', np.where((self.df['outlier flag'] == True) & (self.df['probability score scaled'] >= 0.25), 'medium','low'))
 
-        self.df['risk score'] = prob
-
-        threshold = self.df['risk score'].quantile(0.25)
-
-        self.df['risk level'] = np.where(self.df['risk score'] <= threshold, 'high', 'low')
         iso.fit(self.df)
-        ix = iso.named_steps['preprocessor'].transform(self.df)
-        ix_pca = iso.named_steps['pca'].transform(ix)
-        iso_d = iso.named_steps['iso'].decision_function(ix_pca)
+        x_decision = iso.named_steps['preprocessor'].transform(self.df)
+        x_dec_predict = iso.named_steps['iso'].predict(x_decision)
+        x_dec_function = iso.named_steps['iso'].decision_function(x_decision)
 
-        self.df['anomaly score'] = iso_d
+        self.df['isolation prediction'] = x_dec_predict
+        self.df['isolation outlier flag'] = (self.df['isolation prediction'] == -1)
+        self.df['isolation risk score'] = - x_dec_function
 
-        cutoff = self.df['anomaly score'].quantile(0.25)
+        self.df['isolation risk score scaled'] = MinMaxScaler().fit_transform(self.df['isolation risk score'].values.reshape(-1,1)).ravel()
 
+        self.df['isolation level'] = np.where((self.df['isolation outlier flag'] == True) & (self.df['isolation risk score scaled'] > 0.75), 'high', np.where((self.df['isolation outlier flag'] == True) & (self.df['isolation risk score scaled'] >= 0.25), 'medium','low'))
 
-        self.df['anomaly detector'] = np.where(self.df['anomaly score'] <= cutoff, 'critical', 'low')
+        self.df["Severity Level"] = np.select([(self.df["probability level"] == "high") & (self.df["isolation level"] == "high"),
+                (self.df["probability level"].isin(["high", "medium"])) & (self.df["isolation level"].isin(["high", "medium"]))],["high", "medium"],default="low")
+
         self.df.reset_index(drop=True)
         self.df.insert(0, 'id', self.df.index + 1)
+        print(self.df.head(30).to_string())
+        print()
         #self.df.to_csv('c:/Users/anton/unsuper/cluster/final.csv', index=False)
-        print(self.df.head(1).to_string())
+        #print(self.df.head(50).to_string())
 
         #print(iso_d)
 
