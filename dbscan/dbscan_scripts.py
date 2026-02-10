8c2ee257eb77195636c08414fbfea0c0a567ef19
Antonio Park <antonio.park0517@outlook.com>
2026-02-10 15:47:43 -0500

    Update dbscan_scripts.py

--- dbscan/dbscan_scripts.py
+++ dbscan/dbscan_scripts.py
@@ -29,7 +29,7 @@ class A:
 
 
             self.num = self.copy.select_dtypes(include=['number']).columns
-            self.obj = self.copy.select_dtypes(include=['object']).columns
+            self.obj = self.copy.select_dtypes(include=['object','string']).columns
 
             self.mm = MinMaxScaler()
             self.ohe = OneHotEncoder(drop=None, handle_unknown='ignore', sparse_output=False)
@@ -55,56 +55,32 @@ class A:
 
     def b(self):
         try:
+            self.copy = self.copy.sample(frac=0.2, random_state=42)
             x = self.preprocessor.fit_transform(self.copy)
-            x_df = pd.DataFrame(x, columns=self.preprocessor.get_feature_names_out())
 
             db_scan = DBSCAN(eps=0.5, min_samples=6, metric='minkowski', p=2, algorithm='auto', n_jobs=-1)
 
-            db_scan.fit(x_df)
+            label = db_scan.fit_predict(x)
 
-            label = pd.Series(db_scan.labels_)
+            l, v = np.unique(label, return_counts=True)
 
-            points = pd.Series(label).value_counts()
-
-            plt.figure(figsize=(10, 8))
-            plt.scatter(points.index[points.index == -1], points[points.index == -1], c='red', s=40, label='Noise')
-            plt.scatter(points.index[points.index != -1], points[points.index != -1], c='green', s=30, label='Cluster')
-            plt.xlabel('INDEX')
-            plt.ylabel('VALUES')
-            plt.legend()
-            plt.title('POINTS')
-            plt.show()
-
-            cluster = pd.Series(points).value_counts()
-
-            c_index = pd.Series(points).index.to_numpy()
-            c_value = pd.Series(points).to_numpy()
+            l_sort = np.sort(l)
 
             plt.figure(figsize=(10,8))
-            plt.scatter(np.arange(len(c_index[c_index == -1])), c_value[c_index==-1], c='red', s=40, label='Noise')
-            plt.scatter(np.arange(len(c_index[c_index != -1])), c_value[c_index !=-1], c='green',s=30, label='Cluster')
+            plt.scatter(l_sort[l==-1], v[l==-1],c='red',s=40, label='NOISE')
+            plt.scatter(l_sort[l!=-1], v[l!=-1], c='green', s=30, label='CLUSTER')
             plt.legend()
-            plt.xlabel('INDEX')
-            plt.ylabel('VALUE')
-            plt.title('CLUSTER')
+            plt.xlabel('CLUSTER LABEL (SORTED)')
+            plt.ylabel('CLUSTER SIZE')
+            plt.title('CLUSTER SIZE DISTRIBUTION: NOISE VS CLUSTER')
             plt.show()
 
             for scores in self.scores:
-                s = scores(x_df, label)
+                s = scores(x, label)
                 print(f'{scores.__name__}: {s}')
 
-            label_scaled = pd.Series(MinMaxScaler().fit_transform(label.to_numpy().reshape(-1,1)).ravel())
-            points_scaled = pd.Series(MinMaxScaler().fit_transform(points.to_numpy().reshape(-1,1)).ravel())
-            cluster_scaled = pd.Series(MinMaxScaler().fit_transform(cluster.to_numpy().reshape(-1,1)).ravel())
+            print(pd.Series(label).describe())
 
-            db_signal = pd.DataFrame({'Label': label,
-                                      'Label scaled': label_scaled,
-                                      'Points': points,
-                                      'Points scaled': points_scaled,
-                                      'Cluster': cluster,
-                                      'Cluster Scaled': cluster_scaled})
-
-            print(db_signal.describe().to_string())
 
         except Exception as e:
             raise RuntimeError(f'invalid dbscan: {e}')
